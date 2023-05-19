dc067a652c4006908704a018ec20dd6f09d86e1c
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-05-19 00:12:53 -0300

    Add files via upload

--- src/componets/calculator.js
+++ src/componets/calculator.js
@@ -1,103 +1,124 @@
-import './calculator.css'
-import React, {useState} from "react"
+import './calculator.css';
+import React, { useState } from "react";
+
 function Calculator() {
+  const buttons = [];
 
-    const buttons = [];
-  
-    const [num, setNum] = useState(0);
-    const [oldnum, setOldnum] = useState(0);
-    const [operator, setOperator] = useState(0);
+  // State variables
+  const [num, setNum] = useState(0); // Current number being entered
+  const [oldnum, setOldnum] = useState(0); // Previous number before an operation
+  const [operator, setOperator] = useState(0); // Operator for the current operation
 
-    const ButonCOntrol = ['AC', '+/-', '%', '÷' ,'7' ,'8' ,'9' ,'x','4' ,'5' ,'6' ,'-' ,'1' ,'2' ,'3' ,'+' ,'0' ,'.' ,'=']
-  
-    for (let i = 0; i < ButonCOntrol.length; i++) {
-      let buttonClass = 'button';
+  const ButtonControl = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', ',', '='];
 
-    if (ButonCOntrol[i] === '0') {buttonClass = 'button-0';
+  // Generate buttons based on ButtonControl array
+  for (let i = 0; i < ButtonControl.length; i++) {
+    let buttonClass = 'button';
+
+    if (ButtonControl[i] === '0') {
+      buttonClass = 'button-0';
     }
-    
-    if (['AC', '+/-', '%', '='].includes(ButonCOntrol[i]))buttonClass= 'button-e';
-    else if ([ '÷' ,'x' ,'-' ,',' ,'+'].includes(ButonCOntrol[i])) {
+
+    if (['AC', '+/-', '%', '='].includes(ButtonControl[i])) {
+      buttonClass = 'button-e';
+    } else if (['÷', 'x', '-', ',', '+'].includes(ButtonControl[i])) {
       buttonClass = 'button-x';
     }
-      buttons.push(<button key={i} name={i} id={i} value={ButonCOntrol[i]} onClick={imputNum}  className={buttonClass} >{ButonCOntrol[i]}</button>);
-    
-    }
-    function imputNum(e) {
-      const input = e.target.value;
-    
-      switch (input) {
-        case 'AC':
-          setNum(0);
-          setOldnum(0);
-          break;
-        case '%':
-          setNum(parseFloat(num) / 100);
-          break;
-        case '+/-':
-          setNum(-parseFloat(num));
-          break;
-        case '+':
-          setOldnum(parseFloat(num));
-          setOperator('+');
-          setNum(0);
-          break;
-        case '-':
-          setOldnum(parseFloat(num));
-          setOperator('-');
-          setNum(0);
-          break;
-        case 'x':
-          setOldnum(parseFloat(num));
-          setOperator('x');
-          setNum(0);
-          break;
-        case '÷':
-          setOldnum(parseFloat(num));
-          setOperator('÷');
-          setNum(0);
-          break;
-        case '=':
-          if (operator === '+') {
-            const resultado = (parseFloat(oldnum) + parseFloat(num)).toFixed(3);
-            setOldnum(0);
-            setNum(resultado);
-          } else if (operator === '-') {
-            const resultado = (parseFloat(oldnum) - parseFloat(num)).toFixed(3);
-            setOldnum(0);
-            setNum(resultado);
-          } else if (operator === 'x') {
-            const resultado = (parseFloat(oldnum) * parseFloat(num)).toFixed(3);
-            setOldnum(0);
-            setNum(resultado);
-          } else if (operator === '÷') {
-            const resultado = (parseFloat(oldnum) / parseFloat(num)).toFixed(3);
-            setOldnum(0);
-            setNum(resultado);
-          }
-          break;
-        default:
-          if (num === 0) {
-            setNum(parseFloat(input));
-          } else {
-            setNum(parseFloat(num) + parseFloat(input));
-          }
-          break;
-      }
-    }
 
-
-    
-    return (
-           
-      
-      <div className="wrapper">
-      <span className="result">{num}</span>
-      {buttons}
-      
-      </div>
-
-    )
+    // Create a button element with event handler and class based on buttonClass
+    buttons.push(
+      <button
+        key={i}
+        name={i}
+        id={i}
+        value={ButtonControl[i]}
+        onClick={inputNum}
+        className={buttonClass}
+      >
+        {ButtonControl[i]}
+      </button>
+    );
   }
-  
-  export default Calculator;
+
+  // Handle button click event
+  function inputNum(e) {
+    const input = e.target.value;
+
+    switch (input) {
+      case 'AC':
+        // Clear all numbers and operator
+        setNum(0);
+        setOldnum(0);
+        break;
+      case '%':
+        // Calculate percentage of current number
+        setNum(parseFloat(num) / 100);
+        break;
+      case '+/-':
+        // Toggle the sign of the current number
+        setNum(-parseFloat(num));
+        break;
+      case '+':
+        // Set the operator to addition and store current number as the previous number
+        setOldnum(parseFloat(num));
+        setOperator('+');
+        setNum(0);
+        break;
+      case '-':
+        // Set the operator to subtraction and store current number as the previous number
+        setOldnum(parseFloat(num));
+        setOperator('-');
+        setNum(0);
+        break;
+      case 'x':
+        // Set the operator to multiplication and store current number as the previous number
+        setOldnum(parseFloat(num));
+        setOperator('x');
+        setNum(0);
+        break;
+      case '÷':
+        // Set the operator to division and store current number as the previous number
+        setOldnum(parseFloat(num));
+        setOperator('÷');
+        setNum(0);
+        break;
+      case '=':
+        // Perform the operation based on the operator
+        if (operator === '+') {
+          const resultado = parseFloat(oldnum) + parseFloat(num);
+          setOldnum(0);
+          setNum(resultado);
+        } else if (operator === '-') {
+          const resultado = parseFloat(oldnum) - parseFloat(num);
+          setOldnum(0);
+          setNum(resultado);
+        } else if (operator === 'x') {
+          const resultado = parseFloat(oldnum) * parseFloat(num);
+          setOldnum(0);
+          setNum(resultado);
+        } else if (operator === '÷') {
+          const resultado = parseFloat(oldnum) / parseFloat(num);
+          setOldnum(0);
+          setNum(resultado.toFixed(3)); // Limit the result to 3 decimal places
+        }
+        break;
+      default:
+        // Handle numeric inputs
+        if (num === 0) {
+          setNum(input);
+        } else {
+          setNum(num + input);
+        }
+        break;
+    }
+  }
+
+  return (
+    <div className="wrapper">
+      <span className="result">{num.toString().replace('.', ',')}</span>
+      {buttons}
+    </div>
+  );
+}
+
+export default Calculator;
